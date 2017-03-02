83b1cf3218443a5aa17e8914c46d13c2db640b0c
Nicolas Hamard <nox.tux@gmail.com>
2017-03-02 18:17:21 -0500

    bis (PS:sorry for multiple meaningless commits)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ server.set('port', (process.env.PORT || 5000));
 server.use(express.static(__dirname + '/public'));
 
 server.get('/index', function(req, res, cb) {
-    res.writeHeader(200, {"Context-type":"text/html"});
+    res.writeHeader(200, {"Content-type":"text/html"});
     res.write("hello world");
     res.end; 
 
