c21736d3ab8ed2d726ce1581dd02d88bf71738c1
Giulia Cantoni <cantoni.g@hotmail.com>
2017-03-11 12:51:08 -0500

    Clean start

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 var express = require('express'); 
-var server = express(); 
+var app = express(); 
 
-server.set('port', (process.env.PORT || 5000)); 
-server.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 5000)); 
+app.use(express.static(__dirname + '/public'));
 
-server.get('/index', function(req, res, cb) {
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended : true }));
+
+app.post('/index', function(req, res, cb){
+    console.log("Someone connected to index");
     res.writeHeader(200, {"Content-type":"text/html"});
-    res.write("hello world");
+    res.write("hello world"); 
     res.end; 
 
     return cb(); 
+}); 
+
+app.listen(app.get('port'), function() {
+    console.log("le serveur ecoute sur %s", app.get('port')); 
 });
 
-server.listen(server.get('port'), function() {
-    console.log("le serveur ecoute sur %s", server.get('port')); 
-});
+
+
